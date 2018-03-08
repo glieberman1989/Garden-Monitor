86c900439229c5b875d32f60210ec7c901e50489
Grant Lieberman <lieberman.grant@gmail.com>
2018-03-08 08:50:02 -0800

    -Use of readSoil function for soil moisture sampling. -Removed minutes and seconds from watering trigger -added support for pump realy trigger

--- README.md
+++ README.md
@@ -2,6 +2,4 @@
 garden health monitor and watering system
 
 To-Do:
--debug serial out, and write, via FTDI for trinket Pro 5V
--debug RTC
 -determine soil moisture threshold
--- src/GardenBot.cpp
+++ src/GardenBot.cpp
@@ -6,11 +6,12 @@
 RTC_DS3231 rtc;
 
 int soilVal_1 = 0; //value for storing moisture value
-int soilPin_1 = A0;//Declare a variable for the soil moisture sensor
-int soilPower = 8;//Variable for Soil moisture Power
-int soilThres = 80;//TBD - threshold value for dry vs. wet soil
+int threshold = 250; //*Change this value based on calibration values
 
-int pumpPower = 4;//relay for turning pump off and on
+const int soilPin_1 = A0;//Declare a variable for the soil moisture sensor
+const int soilPower = 8;//Variable for Soil moisture Power
+const int soilThres = 80;//TBD - threshold value for dry vs. wet soil
+const int pumpPower = 4;//relay for turning pump off and on
 
 const unsigned long interval = 3UL*60UL*60UL*1000UL;//run watering loop every 3 hours
 
@@ -36,20 +37,29 @@ void setup() {
   digitalWrite(pumpPower, LOW);//Set to LOW for pump not running
 }
 
-void loop() {
-  DateTime now = rtc.now();
-
-  while (now.hour() >= 8 && now.hour() <= 18) {// && now.minute() == 00 && now.second() == 00) {//only water between 8AM-6PM
+int readSoil() {
     digitalWrite(soilPower, HIGH);//turn D8 "On"
     delay(10);//wait 10 milliseconds
-    //soilVal_1 = analogRead(soilPin_1);//Read the value form sensor
-    //digitalWrite(soilPower, LOW);//turn D8 "Off"
-    //if (soilVal_1 < soilThres) {//if soil wetness is below the threshold value, start watering
+    soilVal_1 = analogRead(soilPin_1);//Read the SIG value form sensor
+    digitalWrite(soilPower, LOW);//turn D8 "Off"
+    return soilVal_1;//send current moisture value
+}
+
+void loop() {
+
+  DateTime now = rtc.now();
+  int sensorValue;
+  sensorValue = readSoil();
+
+  while (now.hour() >= 8 && now.hour() <= 18) {//only water between 8AM-6PM
+    /*Serial.print("Soil Moisture = ");
+    //get soil moisture value from the function below and print it
+    Serial.println(readSoil());*/
+    if (sensorValue <= threshold) {
       digitalWrite(pumpPower, HIGH);
-      delay(1000);//water for 1 minute
+      delay(60000);//water for 1 minute
       digitalWrite(pumpPower, LOW);
-      delay(1000);
-      digitalWrite(soilPower, LOW);
     }
-    //delay(interval);
+    delay(interval);
+  }
 }
