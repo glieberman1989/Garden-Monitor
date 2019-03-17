5354064c2f8d7ee10f6f49953c971420d25cff95
Grant Lieberman <lieberman.grant@gmail.com>
2019-03-17 16:56:51 -0700

    baro integration work

--- src/GardenBot.cpp
+++ src/GardenBot.cpp
@@ -1,25 +1,28 @@
 #include <Arduino.h>
 
 #include <Wire.h>
-#include "RTClib.h"
+#include <RTClib.h>
+#include <Adafruit_BME280.h>
 
-#define manualPin 3
+#define manualIntPin 3
 #define pumpPower 4
 
-//RTC_DS3231 rtc;
-RTC_PCF8523 rtc;
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+RTC_PCF8523 rtc; //RTC_DS3231 rtc
+Adafruit_BME280 bme; // temp, pressure I2C sensors
 
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-int soilVal_1 = 0; //value for storing moisture value
-const int soilPin_1 = A0;//Declare a variable for the soil moisture sensor
+int soilVal_1 = 0; //variable for storing moisture value
+const int soilPin = A0;//pin for the soil moisture sensor
 const int soilPower = 8;//relay for soil moisture sensor power
-const int threshold = 250;//TBD - threshold value for dry vs. wet soil
+const int soilThreshold = 250;//TBD - threshold value for dry vs. wet soil
 
 //Manual Watering Interrupt Control
 void manualOverride() {
-  if (digitalRead(manualPin) == LOW) {
+  if (digitalRead(manualIntPin) == LOW) {
     digitalWrite(pumpPower, HIGH);
-    Serial.println("Manual Overide -- Pump On");
+    Serial.println("Manual Override -- Pump On");
   }
 }
 
@@ -27,33 +30,70 @@ void setup() {
   Serial.begin(9600);
   delay(3000); // wait for console opening
 
+  //BME280 setup
+  bool statusBME;
+  statusBME = bme.begin();
+  if (! statusBME) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1);
+  }
   //RTC setup
-  if (! rtc.begin()) {
+  bool statusRTC;
+  statusRTC = rtc.begin();
+  if (! statusRTC) {
     Serial.println("Couldn't find RTC");
     while (1);
   }
-  //if (rtc.lostPower()) {
-    //Serial.println("RTC lost power, lets set the time!");
-    //following line sets the RTC to the date & time this sketch was compiled
-    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));//sets RTC time to compile/upload time
 
-//Soil Sensor Power
+  //Soil Sensor Power
   pinMode(soilPower, OUTPUT);//Set D8 as an OUTPUT
-  digitalWrite(soilPower, LOW);//Set to LOW so no power is flowing through the sensor
-//Pump Power
+  digitalWrite(soilPower, LOW);//Set to LOW so no power is flowing through the sensors
+  //Pump Power
   pinMode(pumpPower, OUTPUT);//Set D4 as an OUTPUT
   digitalWrite(pumpPower, LOW);//Set to LOW for pump not running
-//Watering Manual Overide
-  pinMode(manualPin, INPUT);
-  attachInterrupt(digitalPinToInterrupt(manualPin), manualOverride, LOW);
+  //Watering Manual Overide
+  pinMode(manualIntPin, INPUT);
+  attachInterrupt(digitalPinToInterrupt(manualIntPin), manualOverride, LOW);
 }
+
 //Soil Moisture Measurment Function
 int readSoil() {
-    digitalWrite(soilPower, HIGH);//turn D8 "On"
-    delay(10);//wait 10 milliseconds
-    soilVal_1 = analogRead(soilPin_1);//Read the SIG value form sensor
-    digitalWrite(soilPower, LOW);//turn D8 "Off"
-    return soilVal_1;//send current moisture value
+  digitalWrite(soilPower, HIGH);//turn D8 "On"
+  delay(10);//wait 10 milliseconds
+  soilVal_1 = analogRead(soilPin);//Read the SIG value form sensor
+  digitalWrite(soilPower, LOW);//turn D8 "Off"
+  return soilVal_1;//send current moisture value
+}
+
+//BME280 Measurment Function
+int readBME280() {
+  return soilVal_1;//send current moisture value
+}
+
+//Serial Print for debugging
+void debugPrint() {
+  //Debug for BME280
+  Serial.print("Temperature = ");
+  Serial.print(bme.readTemperature());
+  Serial.println(" *C");
+  Serial.print("Pressure = ");
+  Serial.print(bme.readPressure() / 100.0F);
+  Serial.println(" hPa");
+  Serial.print("Approx. Altitude = ");
+  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.println(" m");
+  Serial.print("Humidity = ");
+  Serial.print(bme.readHumidity());
+  Serial.println(" %");
+  Serial.println();
+
+  //Debug for moisture sensor
+  Serial.print("Soil Moisture = ");
+  Serial.println(readSoil());
+  Serial.println();
+
+  delay(500);
 }
 
 void loop() {
@@ -61,9 +101,8 @@ void loop() {
   int soilValue;
   soilValue = readSoil();
 
-  //Serial debug for moisture sensor
-  /*Serial.print("Soil Moisture = ");
-  Serial.println(readSoil());*/
+  //Serial debug for sensors
+  debugPrint();
 
   //Serial debug for RTC
   Serial.print(now.year(), DEC);
@@ -80,13 +119,12 @@ void loop() {
   Serial.print(':');
   Serial.print(now.second(), DEC);
   Serial.println();
-  delay(400);
 
   //Main watering cycle
-  if ((now.hour() == 11 && now.minute() >= 45) && (now.hour() == 11 && now.minute() < 46)) {//only water between 8AM-6PM
+  if ((now.hour() == 11 && now.minute() >= 45) && (now.hour() == 11 && now.minute() < 46)) {//water at 11:45
     digitalWrite(pumpPower, HIGH);
     Serial.println("pump ON");
-    delay(500);
+    delay(100);
   }
   else {
     digitalWrite(pumpPower, LOW);
